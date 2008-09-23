f4f443134f4e8bf34d41ad95d561057229ee390c
frink <frink@2681a3e2-908e-4788-b1bb-9ab49f4f0093>
2008-09-23 13:22:24 +0000

    adding these two files
    git-svn-id: https://farnsworth.simcop2387.info/subversion/frinkbot@44 2681a3e2-908e-4788-b1bb-9ab49f4f0093

---
+++ modules/lib/Math/Farnsworth/FunctionDispatch.pm
@@ -0,0 +1,43 @@
+package Math::Farnsworth::FunctionDispatch;
+
+use strict;
+use warnings;
+
+sub new
+{
+	my $self = {};
+	bless $self, (shift);
+}
+
+sub addfunc
+{
+	my $self = shift;
+    my $fundef = shift; #this should be a parse tree one
+	my $name = $fundef->[0];
+	my $args = $fundef->[1];
+	my $value = $fundef->[2];
+
+	#i should really have some error checking here
+	$self->{funcs}{$name} = {name=>$name, args=>$args, value=>$value};
+}
+
+sub getfunc
+{
+	my $self = shift;
+	my $name = shift; #which one to get, we return the hashref
+	return $self->{funcs}{$name};
+}
+
+#should i really have this here? or should i have it in evaluate.pm?
+sub evalfunc
+{
+}
+
+#this should check for correctness of types and such, todo later
+#also should check for number of params
+sub checkparams 
+{
+	return 1;
+}
+
+1;
