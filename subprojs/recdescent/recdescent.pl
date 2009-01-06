7d8d43d6cdc369732ea8a496c83a6bc53850747c
frink <frink@2681a3e2-908e-4788-b1bb-9ab49f4f0093>
2009-01-06 23:48:02 +0000

    got precedence working, added arrays, need to add some more things still
    git-svn-id: https://farnsworth.simcop2387.info/subversion/frinkbot@282 2681a3e2-908e-4788-b1bb-9ab49f4f0093

--- subprojs/recdescent/recdescent.pl
+++ subprojs/recdescent/recdescent.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -w
 
  use strict;
- use Parse::RecDescent;
+use warnings;
+use Parse::RecDescent;
  use Data::Dumper;
 
  use vars qw(%VARIABLE);
@@ -14,32 +15,75 @@
  
  my $grammar = << 'HEREDOCS' ;
 
-   # Terminals (macros that can't expand further)
-   #
+startrule: stmtlist /$/ { $item[1] }
+	| <error: at $text>
 
-   OPL      : m([-+])         # Mathematical operators
-   OPH		: m([*/%])        #higher precedence operators
-   INTEGER  : /[-+]?\d+/      # Signed integers
-   VARIABLE : /[a-z_][a-z0-9_]*/i # Variable
+stmtlist: stmt morestmt(s?) (';')(?) { bless [ $item[1], @{$item[2]} ], 'Stmt' }
 
-   token : VARIABLE {return $main::VARIABLE{$item{VARIABLE}}}
-                    | INTEGER
+morestmt: ';' stmt
 
-   lowerorder  : <leftop: token /(\-|\+)/ token>
-         		 { bless [@item], 'LOWORDER' }
-			   | token
+stmt	: /while\b/ '(' lexpr ')' '{' stmtlist '}'
+                         { bless [ @item[3,6] ], 'While' }
+		| /print\b/ expr { bless [ $item[2] ], 'Print' }
+		| name '=' expr  { bless [ @item[1,3] ], 'Store' }
+		| expr
 
-   expression : lowerorder
+lexpr	: expr '<' expr { bless [ @item[1,3] ], 'Lt' }
+		| expr '>' expr { bless [ @item[1,3] ], 'Gt' }
+		| expr '<=' expr { bless [ @item[1,3] ], 'Le' }
+		| expr '>=' expr { bless [ @item[1,3] ], 'Ge' }
+		| expr '==' expr { bless [ @item[1,3] ], 'Eq' }
+		| expr '!=' expr { bless [ @item[1,3] ], 'Ne' }
+		| expr '&&' expr { bless [ @item[1,3] ], 'And' }
+		| expr '||' expr { bless [ @item[1,3] ], 'Or' }
+		| expr '^^' expr { bless [ @item[1,3] ], 'Xor' }
+		| '!' expr { bless [ @item[1,3] ], 'Not' }
+#		| expr { bless [ $item[1], bless [ 0 ], 'Num' ], 'Ne' } #do i need this?
 
-   print_instruction  : /print/i expression
-                      { print $item{expression}."\n" }
-   assign_instruction : VARIABLE "=" expression
-                      { print Data::Dumper::Dumper(\%item); $main::VARIABLE{$item{VARIABLE}} = $item{expression} }
+expr	: term moreterm(s?)
+					{
+					my $v = $item[1];
+					for(@{$item[2]}){ $v = bless [ $v, @$_ ], ref }
+					$v;
+					}
 
-   instruction : print_instruction
-               | assign_instruction
+moreterm	: '+' term { bless [ $item[2] ], 'Add' }
+			| '-' term { bless [ $item[2] ], 'Sub' }
 
-   startrule: instruction(s /;/)
+term	: factor morefactor(s?)
+					{
+					my $v = $item[1];
+					for(@{$item[2]}){ $v = bless [ $v, @$_ ], ref }
+					$v;
+					}
+
+morefactor	: '*' factor { bless [ $item[2] ], 'Mul' }
+			| '/' factor { bless [ $item[2] ], 'Div' }
+			| '%' factor { bless [ $item[2] ], 'Mod' }
+			| factor { bless [ $item[1], 'imp' ], 'Mul'}
+
+factor : rawtoken moretokens(s?)
+					{
+					my $v = $item[1];
+					for(@{$item[2]}){ $v = bless [ $v, @$_ ], ref }
+					$v;
+					}
+
+moretokens : '**' rawtoken { bless [ $item[2] ], 'Pow' }
+           | '^'  rawtoken { bless [ $item[2] ], 'Pow' }
+
+rawtoken : number { bless [ $item[1] ], 'Num' }
+		 | '(' expr ')' { bless [ $item[2] ], 'Paren' }
+		 | '[' arrayexpr ']' { bless [ $item[2] ], 'Array' }
+		 | name { bless [ $item[1] ], 'Fetch' }
+
+arrayexpr : expr ',' arrayexpr { [$item[1], @{$item[3]}] }
+          | expr { [$item[1]] }
+
+number	: /\d+(?:\.\d*)?/
+				| /\.\d+/
+
+name	: /[a-z]\w*/i
 
 HEREDOCS
 ;
@@ -61,7 +105,7 @@ HEREDOCS
 # print "print 5*7\n";       $parser->startrule("print 5*7");
 # print "print 2/4\n";       $parser->startrule("print 2/4");
 # print "print 2/4+3/2\n";   $parser->startrule("print 2/4+3/2");
- print "a=1-3+4\n";   $parser->startrule("a=1-3+4");
+print "a=1-3+4\n";  print Dumper($parser->startrule("[1,2,3]"));
 # print "print 2+2/4\n";     $parser->startrule("print 2+2/4");
 # print "print 2+-2/4\n";    $parser->startrule("print 2+-2/4");
 # print "a = 5 ; print a\n"; $parser->startrule("a = 5 ; print a");
