8a7d0f8d0c53fcb75847391c3180df818037920a
frink <frink@2681a3e2-908e-4788-b1bb-9ab49f4f0093>
2009-08-20 15:20:37 +0000

    changing the currency module to the one for webservicex.net since for some fucking reason xe.com is unroutable to me... wtf.
    git-svn-id: https://farnsworth.simcop2387.info/subversion/frinkbot@329 2681a3e2-908e-4788-b1bb-9ab49f4f0093

--- trunk/lib/Math/Farnsworth/Units/Currency.pm
+++ trunk/lib/Math/Farnsworth/Units/Currency.pm
@@ -10,24 +10,24 @@ use Math::Farnsworth::Value::Pari;
 use Math::Farnsworth::Units;
 use Math::Farnsworth::Error;
 
+use Finance::Currency::Convert::WebserviceX;
+#use Finance::Currency::Convert::XE;
 
-use Finance::Currency::Convert::XE;
-
-{#this is a REALLY BAD THING TO DO BUT I DON'T WANT NO ROUNDING AT THIS LEVEL
-	no warnings;
-	package Finance::Currency::Convert::XE;
-
-	sub _format
-	{
-		return "%f";
-	}
-}
+#{#this is a REALLY BAD THING TO DO BUT I DON'T WANT NO ROUNDING AT THIS LEVEL
+#	no warnings;
+#	package Finance::Currency::Convert::XE;
+#
+#	sub _format
+#	{
+#		return "%f";
+#	}
+#}
 
 #note that this is fairly C<en-US> centric!
 
 my $defaultcurrency = "USD";
 
-my $obj = Finance::Currency::Convert::XE->new(target => $defaultcurrency)
+my $obj = Finance::Currency::Convert::WebserviceX->new()
                 || die "Failed to create object\n" ;
 
 my @currencies = $obj->currencies; #go get a list of symbols
@@ -59,12 +59,8 @@ sub doupdate
 	for my $x (@currencies)
 	{
 		print "Fetching currency $x\n";
-		my $currentval = $obj->convert(
-                    'value' => '1.00',
-                    'source' => $x,
-					'format' => 'number'
-           )   || die "Could not convert: " . $obj->error . "\n";
-		$env->eval("$x := $currentval dollars;");
+		my $currentval = $obj->convert(100000, $x, "USD") || die "Could not convert\n";
+		$env->eval("$x := $currentval / 100000. dollars;");
 	}
 
 	for my $name (keys %symbols)
@@ -78,7 +74,8 @@ sub doupdate
 		}
 	}
 
-	$Math::Farnsworth::Units::lock = $lock;
+#ignore this for now, it was a bad design anyway
+#	$Math::Farnsworth::Units::lock = $lock;
 
 	return undef;
 }
