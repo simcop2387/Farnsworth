076a3121d899d76b54c7fe806887ff277a463394
Ryan Voots <simcop2387@simcop2387.info>
2010-04-12 14:25:13 -0400

    getting ready to work on some things getting env prepared, about to do some really funky lexing shit

---
+++ trunk/lib/Language/Farnsworth/Functions/Calculus.pm
@@ -0,0 +1,16 @@
+package Language::Farnsworth::Functions::Calculus;
+
+use strict;
+use warnings;
+
+use Math::Pari;
+use Data::Dumper;
+use Language::Farnsworth::Value::Pari;
+use Language::Farnsworth::Value::Array;
+use Language::Farnsworth::Value::Lambda;
+use Language::Farnsworth::Value::Boolean;
+use Language::Farnsworth::Error;
+
+sub init
+{
+}
