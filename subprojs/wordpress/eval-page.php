3458d20415a0bf7d856cace824251694fa1e14ab
Ryan Voots <simcop2387@simcop2387.info>
2010-08-25 22:04:10 -0400

    adding some things that i don't know if they should be here but why the hell not

---
+++ subprojs/wordpress/eval-page.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Eval Page
+Plugin URI: http://simcop2387.info/
+Description: Provides the evaluation interface for the site
+Version: 0.5
+Author: Ryan Voots
+Author URI: http://simcop2387.info
+License: GPL2
+*/
+?>
+
